7065428efb5b16b5dc8e6ec353ce40ba0fa9efb5
antoniorollandee <antoniorollande@gmail.com>
2026-01-29 19:21:33 +0300

    suppression vendor

--- src/Service/TestGenerator.php
+++ src/Service/TestGenerator.php
@@ -1,24 +1,38 @@
-<?php 
+<?php
 
 namespace AiTestBundle\Service;
+
+use Symfony\Component\Routing\Attribute\Route as RouteAttribute;
 
 class TestGenerator
 {
     public function generate(string $controller, array $methods): void
     {
-        $short = (new \ReflectionClass($controller))->getShortName();
+        $refClass = new \ReflectionClass($controller);
+        $short = $refClass->getShortName();
         $testClass = $short . 'Test';
         $path = 'tests/Controller/' . $testClass . '.php';
 
-        $code = "<?php\n\nuse Symfony\\Bundle\\FrameworkBundle\\Test\\WebTestCase;\n\n";
+        $code = "<?php\n\n";
+        $code .= "use Symfony\\Bundle\\FrameworkBundle\\Test\\WebTestCase;\n\n";
         $code .= "class $testClass extends WebTestCase\n{\n";
 
-        foreach ($methods as $method) {
-            $code .= "    public function test" . ucfirst($method) . "()\n    {\n";
-            $code .= "        \$client = static::createClient();\n";
-            $code .= "        \$client->request('GET', '/');\n";
-            $code .= "        \$this->assertResponseIsSuccessful();\n";
-            $code .= "    }\n\n";
+        foreach ($refClass->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
+            foreach ($method->getAttributes(RouteAttribute::class) as $attr) {
+                $route = $attr->newInstance();
+
+                $url = $route->getPath();
+                $httpMethods = $route->getMethods() ?: ['GET'];
+                $httpMethod = $httpMethods[0];
+
+                $testName = 'test' . ucfirst($method->getName());
+
+                $code .= "    public function $testName()\n    {\n";
+                $code .= "        \$client = static::createClient();\n";
+                $code .= "        \$client->request('$httpMethod', '$url');\n";
+                $code .= "        \$this->assertResponseIsSuccessful();\n";
+                $code .= "    }\n\n";
+            }
         }
 
         $code .= "}\n";
